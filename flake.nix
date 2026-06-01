b745f8ade7a7691100e6c8634ab0ea17afa4fb68
Tom Sydney Kerckhove <syd@cs-syd.eu>
2026-06-01 13:08:26 +0200

    Upgrade to nixpkgs version nixos-26.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     crane.url = "github:ipetkov/crane";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     tokei = {
